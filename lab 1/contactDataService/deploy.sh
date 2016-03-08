8b300a7216e759a5d3b96e85aa5a2980e6b4261e
Jignesh Sheth <jsheth@pivotal.io>
2016-03-08 09:42:18 -0600

    upload

--- lab 1/contactDataService/deploy.sh
+++ lab 1/contactDataService/deploy.sh
@@ -6,10 +6,10 @@ function jsonValue() {
   awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p
 }
 
-mvn clean package -DskipTests
+# mvn clean package -DskipTests
 cf t
 echo -n "Validate the space & org, you are currently logged in before continuing!"
 read
-cf cs p-mysql 512mb p-mysql
-cf cs p-rabbitmq standard p-rabbitmq
-cf p -f ./manifest-contactDataService.yml
+cf cs p-mysql 100mb p-mysql
+cf cs cloudamqp lemur p-rabbitmq
+cf p
