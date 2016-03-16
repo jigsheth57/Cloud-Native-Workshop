9b4ed210ad64ca4d63ae2e2cb3f11e11d49c0a5d
Jignesh Sheth <jsheth@pivotal.io>
2016-03-16 11:46:18 -0500

    update

--- lab1/contactDataService/deploy.sh
+++ lab1/contactDataService/deploy.sh
@@ -13,3 +13,10 @@ read
 cf cs p-mysql 100mb p-mysql
 cf cs cloudamqp lemur p-rabbitmq
 cf p
+
+# Generate User Specified Service Instance for this app, so other apps can easyily locate such as contactWebApp
+appdomain=`cf curl /v2/domains | jsonValue name 1 | sed -e 's/^[[:space:]]*//'`
+A_GUID=`cf curl /v2/apps?q=name:contactDataService | jsonValue guid 1 | sed -e 's/^[[:space:]]*//'`
+app_host=`cf curl /v2/apps/${A_GUID}/routes  | jsonValue host 1 | sed -e 's/^[[:space:]]*//'`
+
+cf cups contact-service -p \"{\"tag\":\"contact-service\",\"uri\":\"http://$app_host.$appdomain\"}\"
--- lab2/contactWebApp/deploy.sh
+++ lab2/contactWebApp/deploy.sh
@@ -6,17 +6,8 @@ function jsonValue() {
   awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p
 }
 
-if [ -n "$1" ]
-then
   # mvn clean package -DskipTests
   cf t
   echo -n "Validate the space & org, you are currently logged in before continuing!"
   read
   cf cs cloudamqp lemur p-rabbitmq
-  csJSONStr={\"tag\":\"contact-service\",\"uri\":\"$1\"}
-  echo $csJSONStr
-  cf cups contact-service -p ${csJSONStr}
-  cf p 
-else
-  echo "Usage: deploy <contactDataService URI>"
-fi
