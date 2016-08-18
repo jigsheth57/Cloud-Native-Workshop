ff0374f9691723392fd2378cad58a151e883e112
Jignesh Sheth <jsheth@pivotal.io>
2016-08-18 11:39:02 -0500

    update

--- lab1/ci/tasks/cf-deployment.sh
+++ lab1/ci/tasks/cf-deployment.sh
@@ -6,7 +6,8 @@ echo "Login to PWS ....."
 
 cf login -a $API_ENDPOINT -u $USERNAME -p $PASSWORD -o $ORG -s $SPACE --skip-ssl-validation
 
-DEPLOYED_VERSION_CMD=$(CF_COLOR=false cf a | grep $CF_APP_NAME- | cut -d" " -f1| cut -d"-" -f2)
+# DEPLOYED_VERSION_CMD=$(CF_COLOR=false cf a | grep $CF_APP_NAME- | cut -d" " -f1| cut -d"-" -f2)
+DEPLOYED_VERSION_CMD=$(CF_COLOR=false cf routes | grep $CF_ROUTE_NAME | awk '{ print $4; }' | cut -d"," -f1 | cut -d"-" -f2)
 DEPLOYED_VERSION="$DEPLOYED_VERSION_CMD"
 echo "Deployed Version: $DEPLOYED_VERSION"
 CURRENT_VERSION="blue"
