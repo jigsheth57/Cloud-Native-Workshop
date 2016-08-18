a1d6c51625ecb9f67d1633638174859a06c4e5c6
Jignesh Sheth <jsheth@pivotal.io>
2016-08-18 00:22:53 -0500

    update

--- lab1/ci/tasks/unit-test.sh
+++ lab1/ci/tasks/unit-test.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -e
+version=`cat version/number`
 
 rabbitmq-server start -detached
 
 cd lab-repo/lab1/contactDataService
 
-mvn clean package -DskipTests=true -Djava.version=1.7
+mvn clean versions:set -DnewVersion=$version
+mvn package -DskipTests=true -Djava.version=1.7
 
 rabbitmqctl status
 
